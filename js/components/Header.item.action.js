99d13c88c6042b8787ed982ca098e772c68fbce0
OlegNikitin75 <70277674+OlegNikitin75@users.noreply.github.com>
2021-11-29 13:48:08 +0300

    добавил скрытие виджета погоды

--- js/components/Header.item.action.js
+++ js/components/Header.item.action.js
@@ -1,11 +1,25 @@
 const itemAction = () => {
+  const weatherBtn = document.querySelector('.btn__weather');
+  const weather = document.querySelector('.weather')
   const playerBtn = document.querySelector('.btn__player ');
   const player = document.querySelector('.player');
   const btnClose = document.querySelector('.player__btn-close');
   const slider = document.querySelector('.slider__container');
   const boxSliderControls = document.querySelector('.slider-btn');
+  let isVisibleWeather = true;
   let isVisible = true;
   let isVisibleSlider = true;
+    const visibleHiddenWeather = () => {
+      if (isVisibleWeather) {
+        weather.classList.add('weather--active');
+        isVisibleWeather = false;
+        weatherBtn.style.left = 'calc(100% + 30px)';
+      } else {
+        weather.classList.remove('weather--active');
+        isVisibleWeather = true;
+        weatherBtn.style.left = 0;
+      }
+  };
   const visibleHiddenPlayer = () => {
     if (isVisible) {
       player.classList.add('player--active');
@@ -31,11 +45,8 @@ const itemAction = () => {
       isVisibleSlider = true;
     }
   };
-  playerBtn.addEventListener('click', () => {
-    visibleHiddenPlayer();
-  });
-  btnClose.addEventListener('click', () => {
-    visiblePlayerSlider();
-  });
+  weatherBtn.addEventListener('click', visibleHiddenWeather);
+  playerBtn.addEventListener('click', visibleHiddenPlayer);
+  btnClose.addEventListener('click', visiblePlayerSlider);
 };
 export default itemAction;
